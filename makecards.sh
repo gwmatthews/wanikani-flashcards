fd1340323d4c5a2eb1bbc0fa4741d6ae0375c39e
GWMatthews <george.w.matthews@protonmail.com>
2020-12-09 10:42:11 -0500

    clean up, minor edits

--- makecards.sh
+++ makecards.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###  generates Japanese flashcards from csv files produced by Wanikani Item Inspector
 ###  
-#
+
 function usage {
 
 echo "$0 <filename>" 
@@ -12,57 +12,59 @@ if [ $# -lt 1 ]; then
 	    usage
 	  	exit 1 # error
 fi
-#
+
 ## takes one argument filename of csv file with or without .csv extension
 FILENAME=$1
-#
+
 ## set default font size: can be changed with optional second parameter 30 is good for longer vocab, 55 is good for single kanji
-#
-#FSIZE=${2:-30}                                                             
-#FONTSIZE="\renewcommand{\smallkanji}{\centering\fontsize{$FSIZE}{$FSIZE}}"
+
 SET="${FILENAME%.[^.]*}" 
-#
+
 ## get LaTeX wrappers from card-frame file
-#                 
+
 BEGINFRONT=$(sed -n '2,4 p' < templates/card-frame)
 BEGINBACK=$(sed -n '5,8 p' < templates/card-frame)
 END=$(sed -n '9,11 p' < templates/card-frame)
-#
+
 ## fill content into front of card: wrap comma separated fields in braces, add prefix, divide into sections, same for back
-#
+
 FRONT=$(cat $SET.csv | sed -e 's_Unavailable_{radical_'| sed -e 's/["]/{/' -e 's/[",]./}{/' -e 's/[,"]./}{/' -e 's/"/}/' | sed -e 's/,vocabulary/{}/' | sed -e 's/,radical/{}/' | sed -e 's/,kanji/{k}/'| sed 's/{/\\flashFront{/' | sed -f templates/spacing.sed | sed '/3/ a\\\RLmulticolcolumns')
+
 BACK=$(cat $SET.csv | sed -f templates/kana.sed | sed -e 's_Unavailable_{radical_'| sed -e 's/["]/{/' -e 's/[",]./}{/' -e 's/[,"]./}{/' -e 's/"/}/' | sed -e 's/,vocabulary/{}/' | sed -e 's/,radical/{}/' | sed -e 's/,kanji/{}/' | sed 's/{/\\flashBack{/' | sed -f templates/spacing.sed | sed '/3/ a\\\LRmulticolcolumns')
-#
+
+# COMMENT OUT THE PREVIOUS LINE AND UNCOMMENT THE FOLLOWING TO CHANGE KANJI READINGS TO HIRAGANA
+
+#BACK=$(cat $SET.csv | sed -e 's_Unavailable_{radical_'| sed -e 's/["]/{/' -e 's/[",]./}{/' -e 's/[,"]./}{/' -e 's/"/}/' | sed -e 's/,vocabulary/{}/' | sed -e 's/,radical/{}/' | sed -e 's/,kanji/{}/' | sed 's/{/\\flashBack{/' | sed -f templates/spacing.sed | sed '/3/ a\\\LRmulticolcolumns')
+
+
 ## add front and back of card LaTeX wrappers
-#
-#echo "$FONTSIZE" $'\n' "$BEGINFRONT" "$FRONT" "$END" > $SET-cards-front.tex
-#echo "$FONTSIZE" $'\n' "$BEGINBACK" "$BACK" "$END"  > $SET-cards-back.tex
-#
+
 echo "$BEGINFRONT" "$FRONT" "$END" > $SET-cards-front.tex
 echo "$BEGINBACK" "$BACK" "$END"  > $SET-cards-back.tex
+
 ## duplicate the cards template
-#
+
 cp templates/cards $SET-cards.tex
-#
+
 ## create temp pdf sheets to be reshuffled and rebuilt in code that follows
-#
+
 xelatex $SET-cards.tex
-#
+
 ## reshuffle pages for two sided printing
-#
+
 ## initialize variables
-#
-PAGES=$(pdfinfo $SET-cards.pdf | grep "Pages" | awk '{print $NF}')    ## Extract number of pages in cards file                                                                           
+
+PAGES=$(pdfinfo $SET-cards.pdf | grep "Pages" | awk '{print $NF}')    
 REVERSE=$(( PAGES / 2 ))
-OUTPUT=pagelist.tex                                                   ## For building reshuffled page set
+OUTPUT=pagelist.tex                                                   
 NUM=0
-#
+
 ## copy the template
-#
+
 cp templates/remix $SET.tex                                                 
-#
+
 ## construct the pagelist array
-#
+
 arr=()                                                                
 for ((i=1; i<=$REVERSE; i++)); do
     NUM=$i
@@ -70,25 +72,25 @@ for ((i=1; i<=$REVERSE; i++)); do
     NUM=$(( REVERSE + i))
     arr+=("$NUM" )
 done
-#
+
 ##### format for pdfpages
-#
+
 PAGELIST=$(echo ${arr[@]} | sed 's/\s/, /g')                          
-#
+
 ## assemble full line of LaTeX code
-#
+
 REORDERED="\includepdf[pages={${PAGELIST}}, delta=0 0, offset=0 0]{$SET-cards.pdf}"   
-#
+
 ## export as pagelist.tex
-#
+
 echo $REORDERED > $OUTPUT                                             
-#
+
 ## compile reordered flashcard pages
-#
+
 xelatex $SET                                                     
-#
+
 ## clean up the mess
-#
+
 rm ./*.aux
 rm ./*.log
 rm ./*.out
